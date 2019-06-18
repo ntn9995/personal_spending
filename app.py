2c0e4f009f6dca02941f82cb2b3ef278c73a4f4a
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-06-18 19:00:15 -0400

    refactor parsing email into separate file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import re
 from flask import Flask, jsonify, request
 from sheets import Sheets
 import constants
+import email_parser
 
 app = Flask(__name__)
 
@@ -14,56 +15,12 @@ def parseEmail():
     sheet = Sheets()
     print(repr(body))
 
-    if "You paid" in subject:
-        description, amount = parseVenmo(body, "You paid")
-        category = assignCategory(description, constants.VENMO)
-        method = "Venmo"
-        sheet.addExpense(amount, description, category, method)
-
-    elif "charge request" in subject:
-        description, amount = parseVenmo(body, "charge requst")
-        category = assignCategory(description, constants.VENMO)
-        method = "Venmo"
-        sheet.addExpense(amount, description, category, method)
-
-    elif "Debit card used" in subject:
-        description, amount = parseBofA(body)
-        category = assignCategory(description, constants.BOFA)
-        method = "BofA"
-        sheet.addExpense(description, category, method)
-
-
-def parseVenmo(body, subject="You paid"):
-
-    description = \
-        extract("(?<=\) \\n \\n)(.*)(?= \\n \ T)", body)\
-        if subject == "You paid" else \
-        extract("(?<=\-5\) \\n \\n)(.*)(?= \\n  T)", body)
+    description, amount, category, method = parseEmail.parseEmail(subject, body)
     
-    amount = extract("(?<=\- \$)(.*)(?= \\n     \\n  L)", body)
+    if amount and method:
+        Sheets.addExpense(amount, description, category, method)
+        
 
-    return description, amount
-
-def parseBofA(body):
-    description = "bank of america, food"
-    amount = "$5.00"
-    return description, amount
-
-
-def extract(regex, body):
-    result = re.search(regex, body)
-    return "NOT FOUND" if not result else result.group()
-
-def assignCategory(description, mapping):
-
-    category = "NOT FOUND"
-
-    for key in mapping:
-        if key in description.lower():
-            category = mapping[key]
-            break
-
-    return category
 
 if __name__ == '__main__':
     app.run()
