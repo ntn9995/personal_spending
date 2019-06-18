18131a169e56f808a7538b8e8d6ff93fb863a827
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-06-18 18:10:39 -0400

    constants & sheets

---
+++ app.py
@@ -0,0 +1,4 @@
+import re
+from flask import Flask, jsonify, request
+from sheets import sheets
+import constants
---
+++ constants.py
@@ -0,0 +1,24 @@
+EAT = "Eating Out"
+GRO = "Groceries"
+TRA = "Transporation"
+SUB = "Subscription"
+RNT = "Rent & Utilities"
+
+BOFA = {
+    "spotify": SUB,
+    "amzn digital": SUB,
+    "ting": SUB,
+    "saxbys": EAT,
+    "lyft": TRA,
+    "septa": TRA,
+    "uber": TRA,
+    "freshgrocerwal": GRO
+}
+
+VENMO = {
+    "food": EAT,
+    "internet": RNT,
+    "water": RNT,
+    "electricity": RNT,
+    "gas": RNT
+}
