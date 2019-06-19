4d3f06b393f95a550a42fc312396162c4c9e3258
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-06-18 22:06:45 -0400

    parser cont.

--- parser.py
+++ parser.py
@@ -1,10 +1,10 @@
 class Parser:
 
-        def assignCategory(self, description, mapping):
-                category = "NOT FOUND"
-                for key in mapping:
-                        if key in description.lower():
-                                category = mapping[key]
-                        break
+	def assignCategory(self, description, mapping):
+		category = "NOT FOUND"
+		for key in mapping:
+			if key in description.lower():
+				category = mapping[key]
+			break
 
-                return category
+		return category
