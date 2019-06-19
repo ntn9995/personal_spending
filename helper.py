8b783e04878361e7d833cd923772bd81048a6230
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-06-19 00:12:53 -0400

    change default to "other expenses"

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 class Helper:
 
 	def assignCategory(self, description, mapping):
-		category = "NOT FOUND"
+		category = "Other Expenses"
 		for key in mapping:
 			if key in description.lower():
 				category = mapping[key]
